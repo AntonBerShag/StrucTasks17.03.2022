e4001f531e8fb30690b3f08db7dba661cfe9328d
Ж - 1 < - 1@DESKTOP-V62I8SP>
2022-03-17 21:24:39 +0500

    Add project files.

---
+++ StrucTasks17.03.2022/StrucTasks.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+using namespace std;
+
+struct person {
+	string name;
+	int age;
+	int coins[5];
+};
+
+void showPerson(person &P);
+int yearOfBirth(person& P, int year);
+int cash(person& P);
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	//Задача 1
+	cout << "Задача 1\n";
+	person A = { "Tom", 25, { 5, 1, 1, 2, 5 } };
+	showPerson(A);
+	cout << "Год рождения - " << yearOfBirth(A, 2022) << endl;
+	cout << "Сумма всех монет = " << cash(A) << "\n\n";
+
+	return 0;
+}
+
+//Задача 1
+int cash(person& P) {
+	int sum = 0;
+	for (int i = 0; i < 5; i++)
+		sum += P.coins[i];
+	return sum;
+}
+int yearOfBirth(person& P, int year) {
+	return year - P.age;
+}
+void showPerson(person &P) {
+	cout << P.name << ", " << P.age << " лет.\n";
+	cout << "Монеты: ";
+	for (int i = 0; i < 5; i++)
+		cout << P.coins[i] << ", ";
+	cout << "\b\b.\n";
+}
